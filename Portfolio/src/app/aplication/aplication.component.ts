a5ee0ec2d497a143d2c688460df1b929776d7776
Nahuel Vega <nahuelvegacsharp@gmail.com>
2024-12-27 00:35:29 -0300

    base structure

--- Portfolio/src/app/aplication/aplication.component.ts
+++ Portfolio/src/app/aplication/aplication.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-aplication',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './aplication.component.html',
-  styleUrl: './aplication.component.css'
+  styleUrl: './aplication.component.css',
 })
-export class AplicationComponent {
-
-}
+export class AplicationComponent {}
