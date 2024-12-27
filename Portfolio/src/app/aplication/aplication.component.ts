2aedaa3c9432c1ce03da8eecf424aef14f4214e6
Nahuel Vega <nahuelvegacsharp@gmail.com>
2024-12-27 01:36:25 -0300

    logos and images

--- Portfolio/src/app/aplication/aplication.component.ts
+++ Portfolio/src/app/aplication/aplication.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { Image } from 'primeng/image';
 
 @Component({
   selector: 'app-aplication',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, Image],
   templateUrl: './aplication.component.html',
   styleUrl: './aplication.component.css',
 })
