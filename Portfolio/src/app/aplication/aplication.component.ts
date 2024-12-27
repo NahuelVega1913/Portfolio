be575e3133901ad77c6a998e169aa35836ddbc4e
Nahuel Vega <nahuelvegacsharp@gmail.com>
2024-12-27 16:57:48 -0300

    .

--- Portfolio/src/app/aplication/aplication.component.ts
+++ Portfolio/src/app/aplication/aplication.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { Image } from 'primeng/image';
+import { Card, CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-aplication',
   standalone: true,
-  imports: [ButtonModule, Image],
+  imports: [ButtonModule, Image, Card],
   templateUrl: './aplication.component.html',
   styleUrl: './aplication.component.css',
 })
