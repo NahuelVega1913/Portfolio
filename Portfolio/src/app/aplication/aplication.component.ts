026c534ae72f8c6f62cba709fa529e0bbc725510
Nahuel Vega <nahuelvegacsharp@gmail.com>
2024-12-28 01:03:07 -0300

    .

--- Portfolio/src/app/aplication/aplication.component.ts
+++ Portfolio/src/app/aplication/aplication.component.ts
@@ -3,12 +3,38 @@ import { ButtonModule } from 'primeng/button';
 import { Image } from 'primeng/image';
 import { Card, CardModule } from 'primeng/card';
 import { AnimateOnScroll } from 'primeng/animateonscroll';
+import { FocusTrapModule } from 'primeng/focustrap';
+
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { AutoFocusModule } from 'primeng/autofocus';
 
 @Component({
   selector: 'app-aplication',
   standalone: true,
-  imports: [ButtonModule, Card, CardModule],
+  imports: [
+    ButtonModule,
+    Card,
+    CardModule,
+    FocusTrapModule,
+    ButtonModule,
+    FormsModule,
+    InputTextModule,
+    CheckboxModule,
+    IconFieldModule,
+    InputIconModule,
+    AutoFocusModule,
+  ],
   templateUrl: './aplication.component.html',
   styleUrl: './aplication.component.css',
 })
-export class AplicationComponent {}
+export class AplicationComponent {
+  name: string = '';
+
+  email: string = '';
+
+  accept: boolean = false;
+}
