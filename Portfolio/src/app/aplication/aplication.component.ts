87f80e2edc8b0aa63a8a09ee2e41bb4dcbb108e7
Nahuel Vega <nahuelvegacsharp@gmail.com>
2025-01-04 18:17:22 -0300

    .

--- Portfolio/src/app/aplication/aplication.component.ts
+++ Portfolio/src/app/aplication/aplication.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+
 import { ButtonModule } from 'primeng/button';
 import { Image } from 'primeng/image';
 import { Card, CardModule } from 'primeng/card';
 import { AnimateOnScroll } from 'primeng/animateonscroll';
 import { FocusTrapModule } from 'primeng/focustrap';
 
-import { FormsModule } from '@angular/forms';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  UntypedFormControl,
+  UntypedFormGroup,
+} from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { CheckboxModule } from 'primeng/checkbox';
 import { IconFieldModule } from 'primeng/iconfield';
@@ -14,6 +20,12 @@ import { AutoFocusModule } from 'primeng/autofocus';
 import { Carousel } from 'primeng/carousel';
 import { TagModule } from 'primeng/tag';
 import { CommonModule, NgClass } from '@angular/common';
+import { TextareaModule } from 'primeng/textarea';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { MessageService } from 'primeng/api';
+import { Toast } from 'primeng/toast';
+import { Ripple } from 'primeng/ripple';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-aplication',
@@ -31,20 +43,91 @@ import { CommonModule, NgClass } from '@angular/common';
     IconFieldModule,
     InputIconModule,
     AutoFocusModule,
+    ReactiveFormsModule,
     Carousel,
+    Toast,
+    Ripple,
     TagModule,
+    TextareaModule,
+    IftaLabelModule,
   ],
   templateUrl: './aplication.component.html',
   styleUrl: './aplication.component.css',
+  providers: [MessageService],
 })
 export class AplicationComponent {
   name: string = '';
 
   email: string = '';
+  message: string = '';
 
   accept: boolean = false;
 
   products: undefined[] | undefined;
 
   responsiveOptions: any[] | undefined;
+
+  constructor(private messageService: MessageService) {}
+
+  save() {
+    if (this.name === '' || this.email === '' || this.message === '') {
+      this.showError();
+    } else {
+      const serviceID = 'service_v9ks44m';
+      const templateID = 'template_nz2gl4a';
+      const userID = 'KP045_-SL1KuCwBuw';
+      // Opcional, si tienes un userID
+
+      const templateParams = {
+        from_name: this.name,
+        email_id: this.email,
+        message: this.message,
+      };
+
+      emailjs.send(serviceID, templateID, templateParams, userID).then(
+        () => {
+          this.showSuccess();
+        },
+        (err) => {
+          this.showError();
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'An error occurred, please try again later',
+          });
+        }
+      );
+    }
+  }
+  copyToClipboard() {
+    const textToCopy = 'nahuelvegacsharp@gmail.com';
+    navigator.clipboard
+      .writeText(textToCopy)
+      .then(() => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Success',
+          detail: 'Text copied to clipboard',
+        });
+      })
+      .catch((err) => {});
+  }
+  redirectTo(url: string) {
+    window.open(url);
+  }
+
+  showError() {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: 'Please fill all the fields',
+    });
+  }
+  showSuccess() {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Message sent',
+    });
+  }
 }
--- Portfolio/src/app/app.config.ts
+++ Portfolio/src/app/app.config.ts
@@ -10,6 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
